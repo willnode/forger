b31b99558b02d09df626e9ddb87820f808362ebd
Wildan M <willnode@wellosoft.net>
2022-07-16 21:04:49 +0700

    Init

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Repl from "./repl/Repl.svelte";
+  import bundler from "./repl/workers/bundler/index.js?worker&url";
+  import compiler from "./repl/workers/compiler/index.js?worker&url";
+
+  console.log(bundler);
+</script>
+
+<main>
+  <Repl workersUrl={bundler} svelteUrl={compiler}/>
+</main>
