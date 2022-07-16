951cc4d76e1ff00655fb73c22bd3fca2c1c4edc7
Wildan M <willnode@wellosoft.net>
2022-07-16 21:50:01 +0700

    First working REPL

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,33 @@
 <script>
+import { onMount } from "svelte";
+
+
   import Repl from "../repl/src/Repl.svelte";
+
+  /**
+   * @type {import("../repl/src/Repl.svelte").default}
+   */
+  let repl;
+
+  onMount(function () {
+    repl.set({
+      components: [
+        {
+          name: "App",
+          type: "svelte",
+          source: "<h1>Hello</h1>",
+        },
+      ],
+    });
+  });
 </script>
 
 <main>
-  <Repl workersUrl={'/workers'}/>
+  <Repl
+    bind:this={repl}
+    workersUrl={"/workers"}
+    svelteUrl="https://unpkg.com/svelte@3"
+  />
 </main>
 
 <style>
